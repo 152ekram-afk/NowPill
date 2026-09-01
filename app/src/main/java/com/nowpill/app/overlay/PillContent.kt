14bcf9d28620c091c2a56e6b484507130321c792
MD. Ekramul Islam <152ekram@gmail.com>
2026-09-01 17:59:21 +0900

    Update PillContent.kt

--- app/src/main/java/com/nowpill/app/overlay/PillContent.kt
+++ app/src/main/java/com/nowpill/app/overlay/PillContent.kt
@@ -1,6 +1,7 @@
 package com.nowpill.app.overlay
 
 import androidx.compose.animation.core.*
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
